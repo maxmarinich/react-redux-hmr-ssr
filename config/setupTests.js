e566097eba27f28cf4b965c4991bec438579d7a0
mmarinich <mmarinich@if-mobile23.mara.local>
2018-05-24 15:32:29 +0300

    upd jest config

---
+++ config/setupTests.js
@@ -0,0 +1,9 @@
+import Enzyme from 'enzyme'
+import EnzymeAdapter from 'enzyme-adapter-react-16'
+
+Enzyme.configure({
+  adapter: new EnzymeAdapter(),
+})
+
+// jest mocks here
+//jest.mock('services/SomeService')
--- jest.config.js
+++ jest.config.js
@@ -13,7 +13,7 @@ module.exports = {
   'collectCoverageFrom': [
     'source/client/**/*.{js,jsx,mjs}'
   ],
-  'setupTestFrameworkScriptFile': '<rootDir>src/setupTests.js',
+  'setupTestFrameworkScriptFile': '<rootDir>config/setupTests.js',
   'testMatch': [
     path.resolve(appDirectory, 'source/client/**/__tests__/**/*.{js,jsx,mjs}'),
     path.resolve(appDirectory, 'source/client/**/?(*.)(spec|test).{js,jsx,mjs}'),
